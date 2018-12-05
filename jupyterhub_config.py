c54102dc898cd77a1d5ae86a7a51dbe9b9520b2e
Samuel Tauil <samueltauil@gmail.com>
2018-12-05 09:52:38 -0600

    adding jupyter config file

---
+++ jupyterhub_config.py
@@ -0,0 +1,22 @@
+c.KubeSpawner.environment = dict(JUPYTER_ENABLE_LAB='true')
+
+c.KubeSpawner.user_storage_pvc_ensure = True
+
+c.KubeSpawner.pvc_name_template = '%s-nb-{username}' % c.KubeSpawner.hub_connect_ip
+c.KubeSpawner.user_storage_capacity = '1Gi'
+
+c.KubeSpawner.volumes = [
+    {
+        'name': 'data',
+        'persistentVolumeClaim': {
+            'claimName': c.KubeSpawner.pvc_name_template
+        }
+    }
+]
+
+c.KubeSpawner.volume_mounts = [
+    {
+        'name': 'data',
+        'mountPath': '/opt/app-root/src'
+    }
+]
