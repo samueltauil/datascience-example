e0732d7aa7965e9464244272dffd810cf085fd64
Samuel Tauil <samueltauil@gmail.com>
2018-12-05 14:04:58 -0600

    change line endings

--- deploy_jupyterhub.sh
+++ deploy_jupyterhub.sh
@@ -3,5 +3,5 @@
 oc create -f https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/master/images.json
 oc create -f https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/master/images.json
 oc create -f https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/master/templates.json
-oc new-app --template jupyterhub-quickstart --param APPLICATION_NAME=datascience-example --param GIT_REPOSITORY_URL=https://github.com/samueltauil/datascience-example
+oc new-app --template jupyterhub-quickstart --param APPLICATION_NAME=jupyterhub --param GIT_REPOSITORY_URL=https://github.com/samueltauil/datascience-example
 
