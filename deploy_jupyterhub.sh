65cc266e78669db30df1fded2bec882275ddf36c
Samuel Tauil <samueltauil@gmail.com>
2018-12-05 09:46:53 -0600

    new package

---
+++ deploy_jupyterhub.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+oc create -f https://raw.githubusercontent.com/jupyter-on-openshift/jupyter-notebooks/master/images.json
+oc create -f https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/master/images.json
+oc create -f https://raw.githubusercontent.com/jupyter-on-openshift/jupyterhub-quickstart/master/templates.json
+oc new-app --template jupyterhub-quickstart --param APPLICATION_NAME=datascience-example --param GIT_REPOSITORY_URL=https://github.com/samueltauil/datascience-example
+
